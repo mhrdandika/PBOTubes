164aaf95813703f6292ab71221ec237d8379d820
mhrdandika <mhrdandika@yahoo.com>
2016-03-12 12:00:34 +0700

    new

--- PBO/src/pbo/Orang.java
+++ PBO/src/pbo/Orang.java
@@ -10,5 +10,39 @@ package pbo;
  * @author ASUS
  */
 public class Orang {
+    private String nama;
+    private String nomer;
+    private String email;
+    
+    public Orang (String nama, String nomer, String email){
+        this.nama = nama;
+        this.nomer = nomer;
+        this.email = email;
+                
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    public void setNomer(String nomer){
+        this.nomer = nomer;
+    }
+    
+    public void setEmail(String email){
+        this.nama = nama;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public String getNomer(){
+        return nomer;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
     
 }
